797c87b71644dec03f337c93ac9411926a124226
deepika-1999 <85781588+deepika-1999@users.noreply.github.com>
2021-06-14 16:36:58 +0530

    Add files via upload

---
+++ A01.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p>Number generated randomly is  <?php
+    $rand = rand(1, 10);
+    echo $rand;
+?>.And its square root is 
+<?php
+    echo sqrt($rand);
+?>.</p>
+</body>
+</html>
---
+++ A04.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php   
+$n=371;  
+$sum=0;  
+$i=$n; 
+while($i!=0)  
+{  
+$x=$i%10;  
+$sum=$sum+$x*$x*$x;  
+$i=$i/10;  
+ 
+if($n==$sum)  
+{  
+echo "$n is an Armstrong Number.";  
+}  
+else  
+{  
+echo "$n  not an Armstrong Number.";  
+}  
+?> 
+ 
+</body>
+</html>
